d3d1c045a7109685a06fa90871385d9a156a4e01
Duncan Holm <mail@frou.org>
2015-12-30 22:07:58 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# pg2go
+
+[pg2go] is a basic means for a [PostgreSQL] database to generate [Go] struct
+definitions that correspond to its tables & views.
+
+Here is a shell session demonstrating use:
+
+    DB=blogdb
+    RES="types_$DB.go"
+
+    echo "package main" >"$RES"
+    psql -q -t -A -d "$DB" -f pg2go.sql >>"$RES"   # the noteworthy part
+    goimports -w "$RES" || gofmt -w "$RES"
+
+    head -n 22 types_blogdb.go                     # peek at the resultant file
+    package main
+
+    import (
+        "database/sql"
+        "time"
+    )
+
+    type author struct {
+        Uid       int       `db:"uid"`
+        Created   time.Time `db:"created"`
+        Admin     bool      `db:"admin"`
+        Name      string    `db:"name"`
+        Email     string    `db:"email"`
+        LoginSalt []byte    `db:"login_salt"`
+        LoginKey  []byte    `db:"login_key"`
+    }
+
+    type comment struct {
+        Uid     int       `db:"uid"`
+        Created time.Time `db:"created"`
+        Post    int       `db:"post"`
+        Author  int       `db:"author"`
+
+# Notes
+
+Using [goimports] rather than standard gofmt to format the resultant file has
+the benefit of automatically importing packages iff they are required by what
+was generated (e.g. "time" for time.Time & "database/sql" for sql.NullString).
+
+Struct fields are tagged `db:"..."` for [package sqlx][sqlx] to pick up on,
+should you wish to use it.
+
+If NEED_GO_TYPE_FOR_... shows up in the resultant file then add a case for that
+type name to the TYPE_PG2GO function in the .sql file.
+
+If the tables & views you're interested in aren't in the `'public'` schema then
+search and replace that in the .sql file.
+
+If you want the struct identifiers, and not just their fields, to be exported
+(start with upper case) then search and replace `NAME_PG2GO(table_name, false)`
+with `NAME_PG2GO(table_name, true)` in the .sql file.
+
+[pg2go]: https://github.com/frou/pg2go
+[postgresql]: https://www.postgresql.org
+[goimports]: https://godoc.org/golang.org/x/tools/cmd/goimports
+[go]: https://www.golang.org
+[sqlx]: https://github.com/jmoiron/sqlx
---
+++ pg2go.sql
@@ -0,0 +1,72 @@
+-- https://github.com/frou/pg2go
+
+CREATE FUNCTION NAME_PG2GO(nm TEXT, exported BOOLEAN) RETURNS TEXT AS $$
+  SELECT CASE
+    WHEN exported THEN
+      -- snake_case -> PascalCase
+      REPLACE(INITCAP(REPLACE(nm, '_', ' ')), ' ', '')
+    ELSE
+      -- snake_case -> camelCase
+      LOWER(SUBSTRING(nm, 1, 1)) || SUBSTRING(NAME_PG2GO(nm, true), 2)
+    END
+$$
+LANGUAGE SQL
+IMMUTABLE;
+
+CREATE FUNCTION TYPE_PG2GO(typ TEXT, nullable BOOLEAN) RETURNS TEXT AS $$
+  SELECT CASE
+    WHEN nullable THEN
+      CASE typ
+        WHEN 'BIGINT'           THEN 'sql.NullInt64'
+        WHEN 'BOOLEAN'          THEN 'sql.NullBool'
+        WHEN 'DOUBLE PRECISION' THEN 'sql.NullFloat64'
+        WHEN 'INTEGER'          THEN 'sql.NullInt64'
+        WHEN 'REAL'             THEN 'sql.NullFloat64'
+        WHEN 'TEXT'             THEN 'sql.NullString'
+
+        ELSE 'NEED_GO_TYPE_FOR_NULLABLE_' || REPLACE(typ, ' ', '_')
+      END
+    ELSE
+      CASE typ
+        WHEN 'BIGINT'           THEN 'int'
+        WHEN 'BOOLEAN'          THEN 'bool'
+        WHEN 'DOUBLE PRECISION' THEN 'float64'
+        WHEN 'INTEGER'          THEN 'int'
+        WHEN 'REAL'             THEN 'float32'
+        WHEN 'TEXT'             THEN 'string'
+
+        WHEN 'BYTEA'                       THEN '[]byte'
+        WHEN 'TIMESTAMP WITH TIME ZONE'    THEN 'time.Time'
+        WHEN 'TIMESTAMP WITHOUT TIME ZONE' THEN 'time.Time'
+
+        ELSE 'NEED_GO_TYPE_FOR_' || REPLACE(typ, ' ', '_')
+      END
+  END;
+$$
+LANGUAGE SQL
+IMMUTABLE;
+
+------------------------------------------------------------
+
+WITH struct AS (
+  WITH db_extract AS (
+    SELECT table_name, column_name, data_type, is_nullable
+    FROM information_schema.columns
+    WHERE table_schema = 'public'
+    ORDER BY table_schema, table_name, ordinal_position
+  )
+  SELECT NAME_PG2GO(table_name, false) AS identifier,
+         STRING_AGG(E'\t' || NAME_PG2GO(column_name, true) || ' '
+                          || TYPE_PG2GO(UPPER(data_type), is_nullable::BOOLEAN)
+                          || ' `db:"' || column_name || '"`',
+                    E'\n') AS agg_fields
+  FROM db_extract GROUP BY table_name
+  ORDER BY identifier
+)
+SELECT 'type ' || identifier || E' struct {\n' || agg_fields || E'\n}\n'
+FROM struct;
+
+------------------------------------------------------------
+
+DROP FUNCTION NAME_PG2GO(TEXT, BOOLEAN);
+DROP FUNCTION TYPE_PG2GO(TEXT, BOOLEAN);
